090526aa1415f8f909e40b8cdb168059c4cb7b5f
onethousandworks <books@onethousand.works>
2026-07-01 10:14:24 +0100

    Adiciona SEO e Open Graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://onethousand.works",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  i18n: {
+    defaultLocale: "pt",
+    locales: ["pt", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
